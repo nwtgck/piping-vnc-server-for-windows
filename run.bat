6dd9d4007eabbf3f7811bf172bb2535e24435afb
Ryo Ota <nwtgck@nwtgck.org>
2021-08-08 10:43:28 +0900

    chore

--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
+setlocal
+
 :: Whether winvnc started or not
 set vnc_started=1==2
 
@@ -11,16 +13,18 @@ for /f "usebackq" %%a in (`netstat -ano ^| find "127.0.0.1:5900"`) do @set vnc_s
 if %vnc_status% == NOT_RUNNING (
   if not %vnc_started% (
     start .\UltraVnc_133_dev10\x64\winvnc.exe
-    vnc_started=1==1
+    set vnc_started=1==1
   )
 ) else (
   goto vnc_running
 )
 
-timeout /t 2
+timeout /t 2 > nul
 goto loop
 
 :vnc_running
 
 :: Start tunneling
 .\piping-tunnel\piping-tunnel server -p 5900 XteS20PrNMrjBRk5 KPoNbhFpkHAwRD6k
+
+pause
