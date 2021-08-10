46fa36b19b64c36cc77f8ded8f7b8cba6cc59105
Ryo Ota <nwtgck@nwtgck.org>
2021-08-11 00:04:28 +0900

    update e2ee_info

--- download-page/src/strings.ts
+++ download-page/src/strings.ts
@@ -17,8 +17,8 @@ const en = {
   open_piping_vnc_to_control_remotely: "Open Piping VNC to control remotely",
   e2e_encryption: "E2E encryption",
   e2ee_info: `\
-OpenSSL-compatible AES-CTR-256
-PBKDF2 iterations: 100000, PBKDF2 hash: SHA-256
+End-to-end encryption is conducted using the same way as openssl command does.
+The cipher type is AES-256-CTR and the key is derived by PBKDF2 with iterations = 100000 and hash = SHA-256.
 `,
   e2ee_passphrase: "E2EE passphrase",
   detail_config: "Detail config",
@@ -32,7 +32,10 @@ const ja: typeof defaultStr = {
   gpl_notice_html: `ZIPファイルは <a href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank" class="grey--text">GNU/GPL ライセンス</a>の UltraVNC を含んでいます。`,
   open_piping_vnc_to_control_remotely: "Piping VNCを開いて制御する",
   e2e_encryption: "E2E 暗号化",
-  e2ee_info: en.e2ee_info,
+  e2ee_info: `\
+opensslコマンドと同様の暗号化方法でエンドツーエンド暗号化を行います。
+暗号タイプはAES-256-CTRで、鍵導出はPBKDF2（反復回数＝100000、ハッシュ＝SHA-256）を使用します。
+`,
   e2ee_passphrase: "E2EE パスフレーズ",
   detail_config: "詳細設定",
 };
