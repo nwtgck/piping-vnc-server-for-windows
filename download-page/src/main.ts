ec44f3813c8f2ec9e8a02c1e03c1b83f16d2195c
Ryo Ota <nwtgck@nwtgck.org>
2022-12-08 21:33:15 +0900

    try

--- download-page/src/main.ts
+++ download-page/src/main.ts
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+//
+// new Vue({
+//   vuetify,
+//   render: h => h(App)
+// }).$mount('#app')
 
-new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+console.log("vueity", vuetify);
--- download-page/src/plugins/vuetify.ts
+++ download-page/src/plugins/vuetify.ts
@@ -1,15 +1,32 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import { createApp } from 'vue';
+// import Vuetify from 'vuetify/lib/framework';
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import App from '../App.vue';
 
-Vue.use(Vuetify);
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+// Vue.use(Vuetify);
 
 library.add(faGithub);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
-export default new Vuetify({
-  icons: {
-    iconfont: "mdiSvg"
-  }
-});
+// Vue.component('font-awesome-icon', FontAwesomeIcon);
+// export default new Vuetify({
+//   icons: {
+//     iconfont: "mdiSvg"
+//   }
+// });
+
+// @ts-ignore
+const x = createApp(App)
+  .use(vuetify)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
+
+export default x;
